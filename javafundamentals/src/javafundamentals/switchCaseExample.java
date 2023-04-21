d407d924227be1833688a638b4f8434323937bde
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-04-21 12:37:27 +0800

    update Java Examples

---
+++ javafundamentals/src/javafundamentals/switchCaseExample.java
@@ -0,0 +1,29 @@
+package javafundamentals;
+
+public class switchCaseExample {
+public static void main(String[] args) {
+    int l=12,b=10;
+    double area=0,perimeter=0,diagonal=0;
+    char choice='p';
+    switch(choice) {
+    case 'a': 
+    case 'A':    
+        area=l*b;
+        System.out.println(area);
+        break;        
+    case 'p':
+    case 'P':
+        perimeter=2*(l+b);
+        System.out.println(perimeter);
+        break;
+    case 'd':
+    case 'D':
+        diagonal= Math.sqrt(l*l+b*b);
+        System.out.println(diagonal);
+        break;
+    default:
+        System.out.println("PLZ enter correct choice");
+    }
+    System.out.println("HI"); // Last Line 
+}
+}
---
+++ javafundamentals/src/javafundamentals/vowelConsonant.java
@@ -0,0 +1,16 @@
+package javafundamentals;
+
+public class vowelConsonant {
+	public static void main(String[] args) {
+    char ch='o';
+    switch(ch) {
+    case 'a':     case 'A':    case 'e':     case 'E':    case 'i':    case 'I':    case 'o': 
+        case 'O':    case 'u':    case 'U':    
+        System.out.println("Vowel");
+        break;
+    default:
+        System.out.println("consonant");
+    }
+    System.out.println("HI"); // Last Line 
+}
+}
